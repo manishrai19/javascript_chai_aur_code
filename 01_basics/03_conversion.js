77a6130e369ff32eeb7a1e602555e74ea5f90a6e
manis <manishrai19@gmail.com>
2024-04-01 23:50:43 +0200

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,35 @@
+// let score = 1;
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+/*
+ "33" => 33
+ "33abc" =>Nan
+ true =>1; false =>0
+ */
+
+// let isLoggedIn = 0;
+// let booleanIsLogged = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLogged);
+// console.log(booleanIsLogged);
+
+/*
+ 1 , "Manish" => True
+ "" , 0  => false
+ */
+
+let someNumber = true;
+let stringNumber = String(someNumber);
+
+console.log(someNumber);
+
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
