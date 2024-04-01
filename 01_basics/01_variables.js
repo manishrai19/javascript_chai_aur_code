0edf8d278ac795e52aff0e739c7438aa6bd37b08
manis <manishrai19@gmail.com>
2024-04-01 23:17:19 +0200

    first commint

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = '123456'  
+var accountName = "Manish Rai"
+let accountMail="manishrai19@gmail.com"
+accountCity = "Jaipur"  // Do not use this as this is not correct way
+
+let accountState;  // Semicolon is not mandatory
+
+/*
+var  : Do not use var unless it is specifed because of block and functional scope
+
+*/
+
+// accountId = '1231211232' //not allowed  as this is a constant
+console.log(accountId);
+
+
+
+console.table([accountId,accountName,accountMail,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // we are using node js and not browser hence its not working
+
+/* Types of Data Types 
+
+PRIMITIVE Data Types
+string
+BigInt
+number > 2 to the power 53
+boolean
+null => Standalone value
+undefined => value is not assigned yet
+symbol => for uniqueness
+
+Objects
+
+*/
+
+let age = 3
+let name = "Manish"
+
+console.log(typeof(age));
+console.log(typeof(name));  
+console.log(typeof(undefined));
+
+
